c3ed88982f2519051d95cb6799d234bc7348179e
= <=>
2022-03-06 21:08:39 +0800

    fix vuex persisted state

--- resources/js/api/api.js
+++ resources/js/api/api.js
@@ -2,13 +2,16 @@ import axios from "axios";
 import config from "../constants/config";
 
 const base_url = config.api_endpoint;
-let auth = JSON.parse(localStorage.getItem('vuex')).auth;
+let vuex = JSON.parse(localStorage.getItem('vuex'));
 
 let api = axios.create({
   baseURL: base_url
 });
-if(auth.is_authenticated) {
-    api.defaults.headers.common['Authorization'] = `Bearer: ${auth.active_user.token}`
+
+if(vuex) {
+    if(vuex.auth.is_authenticated) {
+        api.defaults.headers.common['Authorization'] = `Bearer: ${auth.active_user.token}`
+    }
 }
 
 api.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
